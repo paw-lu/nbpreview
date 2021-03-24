e16ae0ada4ba2b920db160d51e8bdee8cfcc51ca
Paulo S. Costa <pauloscosta5@gmail.com>
2021-03-23 20:58:45 -0700

    Add main cli

--- src/nbpreview/__main__.py
+++ src/nbpreview/__main__.py
@@ -1,12 +1,71 @@
 """Command-line interface."""
-import click
+import sys
+from pathlib import Path
+
+import nbformat
+import rich.console
+import rich.traceback
+import typer
+
+from . import __version__
+from . import render
+
+app = typer.Typer()
+rich.traceback.install(theme="ansi_dark", show_locals=True)
 
 
-@click.command()
-@click.version_option()
-def main() -> None:
-    """nbpreview."""
+def version_callback(value: bool) -> None:
+    """Return the package version.
+
+    Args:
+        value (bool): Whether to return the version.
+
+    Raises:
+        Exit: Exits the command line interface with an exit code of 0.
+    """
+    if value:
+        typer.echo(f"nbpreview {__version__}")
+        raise typer.Exit()
+    pass
+
+
+file_argument = typer.Argument(
+    ...,
+    exists=True,
+    file_okay=True,
+    dir_okay=False,
+    readable=True,
+    help="A Jupyter Notebook file to render.",
+)
+theme_option = typer.Option(
+    "ansi_dark", "--theme", "-t", help="The theme to use for syntax highlighting."
+)
+version_option = typer.Option(
+    None,
+    "--version",
+    help="Display the version and exit.",
+    callback=version_callback,
+    is_eager=True,
+)
+
+
+@app.command()
+def main(
+    file: Path = file_argument,
+    theme: str = theme_option,
+    version: bool = version_option,
+) -> None:
+    """Render a Jupyter Notebook in the terminal."""
+    console = rich.console.Console()
+    stderr = rich.console.Console(file=sys.stdout)
+
+    try:
+        notebook = render.Notebook(notebook_path=file, theme=theme, nb_version=4)
+    except nbformat.reader.NotJSONError:
+        stderr.print(f"{file} is not a valid Jupyter Notebook path.")
+        raise typer.Exit(1)
+    console.print(notebook)
 
 
 if __name__ == "__main__":
-    main(prog_name="nbpreview")  # pragma: no cover
+    app()  # pragma: no cover
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,51 @@
 """Test cases for the __main__ module."""
-import pytest
-from click.testing import CliRunner
+import pathlib
 
-from nbpreview import __main__
+import pytest
+import typer.testing
+from typer.testing import CliRunner
+
+import nbpreview
+from nbpreview.__main__ import app
 
 
 @pytest.fixture
 def runner() -> CliRunner:
     """Fixture for invoking command-line interfaces."""
-    return CliRunner()
+    return typer.testing.CliRunner()
 
 
-def test_main_succeeds(runner: CliRunner) -> None:
-    """It exits with a status code of zero."""
-    result = runner.invoke(__main__.main)
+@pytest.fixture
+def notebook() -> str:
+    """Fixture for returning an example notebook."""
+    return str(pathlib.Path(__file__).parent / pathlib.Path("notebook.ipynb"))
+
+
+def test_main_succeeds(runner: CliRunner, notebook: str) -> None:
+    """It exits with a status code of zero with a valid file."""
+    result = runner.invoke(app, [notebook])
     assert result.exit_code == 0
+
+
+def test_version(runner: CliRunner) -> None:
+    """It returns the version number."""
+    result = runner.invoke(app, ["--version"])
+    assert result.stdout == f"nbpreview {nbpreview.__version__}\n"
+
+
+def test_exit_invalid_file_status(runner: CliRunner) -> None:
+    """It exits with a status code of 1 when fed an invalid file."""
+    invalid_file = str(
+        (pathlib.Path(__file__).parent / pathlib.Path("__init__.py")).resolve()
+    )
+    result = runner.invoke(app, [invalid_file])
+    assert result.exit_code == 1
+
+
+def test_exit_invalid_file_output(runner: CliRunner) -> None:
+    """It outputs a message when fed an invalid file."""
+    invalid_file = str(
+        (pathlib.Path(__file__).parent / pathlib.Path("__init__.py")).resolve()
+    )
+    result = runner.invoke(app, [invalid_file])
+    assert result.output == f"{invalid_file} is not a valid \nJupyter Notebook path.\n"
