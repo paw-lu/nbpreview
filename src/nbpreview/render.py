6e72e283ec5522af0f6767d2d069cfe7197113c3
Paulo S. Costa <pauloscosta5@gmail.com>
2021-04-04 17:28:26 -0700

    Render notebook cells

---
+++ src/nbpreview/render.py
@@ -0,0 +1,162 @@
+"""Render the notebook."""
+from typing import Iterator
+from typing import Tuple
+from typing import Union
+
+import pygments
+from nbformat.notebooknode import NotebookNode
+from rich import markdown
+from rich import panel
+from rich import syntax
+from rich import table
+from rich import text
+from rich.console import Console
+from rich.console import ConsoleOptions
+from rich.markdown import Markdown
+from rich.panel import Panel
+from rich.syntax import Syntax
+from rich.table import Table
+from rich.text import Text
+
+Cell = Union[Markdown, Panel, Text, Syntax]
+
+
+class Notebook:
+    """Construct a Notebook object to render Jupyter Notebooks.
+
+    Args:
+        notebook_node (NotebookNode): A NotebookNode of the notebook to
+            render.
+        theme (str): The theme to use for syntax highlighting. May be
+            "ansi_light", "ansi_dark", or any Pygments theme. By default
+            "ansi_dark".
+        plain (bool): Only show plain style. No decorations such as
+            boxes or execution counts. By default False.
+    """
+
+    def __init__(
+        self, notebook_node: NotebookNode, theme: str = "ansi_dark", plain: bool = False
+    ) -> None:
+        """Initialize."""
+        self.cells = notebook_node.cells
+        self.language = notebook_node.metadata.kernelspec.language
+        self.theme = theme
+        self.plain = plain
+
+    def _render_execution_indicator(self, execution_count: Union[str, None]) -> Text:
+        """Render the execution indicator.
+
+        Args:
+            execution_count (Union[str, None]): The execution count.
+                Set to None if there is no execution count.
+
+        Returns:
+            Text: The rendered execution indicator.
+        """
+        if execution_count is None:
+            execution_indicator = ""
+        else:
+            execution_indicator = f"\n[{execution_count}]:"
+        return text.Text(execution_indicator, style="color(247)")
+
+    def _render_cells(self, cell: NotebookNode) -> Tuple[Text, Cell]:
+        """Render a Jupyter Notebook cell.
+
+        Args:
+            cell (NotebookNode): The cell to render
+
+        Returns:
+            Tuple[Text, Cell]: The execution count indicator and cell
+                content.
+        """
+        cell_type = cell.get("cell_type", False)
+        source = cell.source
+        default_lexer_name = "ipython" if self.language == "python" else self.language
+
+        if cell_type == "markdown":
+            execution_count = None
+            rendered_cell: Cell = markdown.Markdown(
+                source, inline_code_theme=self.theme
+            )
+
+        elif cell_type == "code":
+            execution_count = (
+                cell.execution_count if cell.execution_count is not None else " "
+            )
+            rendered_source: Cell = syntax.Syntax(
+                source,
+                lexer_name=default_lexer_name,
+                theme=self.theme,
+                background_color="default",
+            )
+
+            if source.startswith("%%"):
+                try:
+                    magic, body = source.split("\n", 1)
+                    language_name = magic.lstrip("%")
+                    body_lexer_name = pygments.lexers.get_lexer_by_name(
+                        language_name
+                    ).name
+                    # Syntax needs a string in the init, so pass an
+                    # empty string and then pass the actual code to
+                    # highlight method
+                    magic_syntax = syntax.Syntax(
+                        "",
+                        lexer_name=default_lexer_name,
+                        theme=self.theme,
+                        background_color="default",
+                    ).highlight(magic)
+                    body_syntax = syntax.Syntax(
+                        "",
+                        lexer_name=body_lexer_name,
+                        theme=self.theme,
+                        background_color="default",
+                    ).highlight(body)
+                    rendered_source = text.Text().join((magic_syntax, body_syntax))
+
+                except pygments.util.ClassNotFound:
+                    pass
+
+            if not self.plain:
+                rendered_cell = panel.Panel(
+                    rendered_source,
+                    expand=True,
+                )
+            else:
+                rendered_cell = rendered_source
+
+        else:
+            execution_count = None
+            rendered_cell = panel.Panel(source)
+
+        execution_count_indicator = self._render_execution_indicator(execution_count)
+        return execution_count_indicator, rendered_cell
+
+    def __rich_console__(
+        self, console: Console, options: ConsoleOptions
+    ) -> Iterator[Table]:
+        """Render the Notebook to the terminal.
+
+        Args:
+            console (Console): The Rich Console object.
+            options (ConsoleOptions): The Rich Console options.
+
+        Yields:
+            Iterator[RenderResult]: The
+        """
+        grid = table.Table.grid(
+            padding=(0, 1, 0, 0),
+            collapse_padding=True,
+        )
+        if not self.plain:
+            grid.add_column(justify="right")
+        grid.add_column()
+
+        for cell in self.cells:
+            execution_count_indicator, source = self._render_cells(cell)
+            cell_row = (
+                (execution_count_indicator, source) if not self.plain else (source,)
+            )
+            grid.add_row(*cell_row)
+
+        yield grid
---
+++ tests/test_render.py
@@ -0,0 +1,217 @@
+"""Test cases for render."""
+import io
+from typing import Any
+from typing import Callable
+from typing import Dict
+from typing import List
+from typing import Optional
+from typing import Protocol
+
+import pytest
+from nbformat import NotebookNode
+from rich import console
+from rich.console import Console
+
+from nbpreview import render
+
+
+class RichOutput(Protocol):
+    """Typing protocol for _rich_output."""
+
+    def __call__(
+        self,
+        cell: Dict[str, Any],
+        plain: bool = False,
+        no_wrap: bool = False,
+    ) -> str:
+        """Callable types."""
+        ...
+
+
+def split_string(string: str, sub_length: int = 30) -> List[str]:
+    """Split a string into subsections less than or equal to new length.
+
+    Args:
+        string (str): The long string to split up.
+        sub_length (int): The maximum length of the subsections.
+            Defaults to 56.
+
+    Returns:
+        List[str]: The string split into sections.
+    """
+    string_length = len(string)
+    return [
+        string[begin : begin + sub_length]
+        for begin in range(0, string_length, sub_length)
+    ]
+
+
+@pytest.fixture()
+def rich_console() -> Console:
+    """Fixture that returns Rich console."""
+    con = console.Console(
+        file=io.StringIO(),
+        width=80,
+        color_system="truecolor",
+        legacy_windows=False,
+    )
+    return con
+
+
+@pytest.fixture()
+def rich_output(
+    rich_console: Console,
+    make_notebook: Callable[[Optional[Dict[str, Any]]], NotebookNode],
+) -> Callable[..., str]:
+    """Fixture returning a function that returns the rendered output.
+
+    Args:
+        rich_console (Console): Pytest fixture that returns a rich
+            console.
+        make_notebook (Callable[[Optional[Dict[str, Any]]], NotebookNode]):
+            A fixture that creates a notebook node.
+
+    Returns:
+        Callable[..., str]: The output generating function.
+    """
+
+    def _rich_output(
+        cell: Dict[str, Any],
+        plain: bool = False,
+        no_wrap: bool = False,
+    ) -> str:
+        """Return the rendered output of a notebook containing the cell.
+
+        Args:
+            cell (Dict[str, Any]): The cell of the notebook to render.
+            plain (bool): Whether to render the notebook in a
+                plain style, with no boxes or decorations. Defaults to
+                False.
+            no_wrap (bool): Disable word wrapping. Defaults to False.
+
+        Returns:
+            str: The rich output as a string.
+        """
+        notebook_node = make_notebook(cell)
+        notebook = render.Notebook(notebook_node, plain=plain)
+        rich_console.print(notebook, no_wrap=no_wrap)
+        output: str = rich_console.file.getvalue()  # type: ignore[attr-defined]
+        return output
+
+    return _rich_output
+
+
+def test_notebook_markdown_cell(rich_output: RichOutput) -> None:
+    """It renders a markdown cell."""
+    markdown_cell = {
+        "cell_type": "markdown",
+        "id": "academic-bride",
+        "metadata": {},
+        "source": "### Lorep ipsum\n\n**dolor** _sit_ `amet`",
+    }
+    output = rich_output(markdown_cell)
+    expected_output = (
+        "                              "
+        "     \x1b[1mLorep ipsum\x1b[0m      "
+        "                            \n "
+        "                              "
+        "                              "
+        "                   \n \x1b[1mdolor"
+        "\x1b[0m \x1b[3msit\x1b[0m \x1b[97;40mamet\x1b"
+        "[0m                           "
+        "                              "
+        "        \n"
+    )
+    assert output == expected_output
+
+
+def test_notebook_code_cell(rich_output: RichOutput) -> None:
+    """It renders a code cell."""
+    code_cell = {
+        "cell_type": "code",
+        "execution_count": 2,
+        "id": "emotional-amount",
+        "metadata": {},
+        "outputs": [],
+        "source": "def foo(x: float, y: float) -> float:\n    return x + y",
+    }
+    output = rich_output(code_cell)
+    expected_output = (
+        "\x1b[38;5;247m    \x1b[0m ╭─────────"
+        "──────────────────────────────"
+        "──────────────────────────────"
+        "────╮\n\x1b[38;5;247m[2]:\x1b[0m │ \x1b["
+        "94;49mdef\x1b[0m\x1b[49m \x1b[0m\x1b[92;49"
+        "mfoo\x1b[0m\x1b[49m(\x1b[0m\x1b[49mx\x1b[0m\x1b["
+        "49m:\x1b[0m\x1b[49m \x1b[0m\x1b[96;49mfloa"
+        "t\x1b[0m\x1b[49m,\x1b[0m\x1b[49m \x1b[0m\x1b[49m"
+        "y\x1b[0m\x1b[49m:\x1b[0m\x1b[49m \x1b[0m\x1b[96;"
+        "49mfloat\x1b[0m\x1b[49m)\x1b[0m\x1b[49m \x1b["
+        "0m\x1b[49m-\x1b[0m\x1b[49m>\x1b[0m\x1b[49m \x1b["
+        "0m\x1b[96;49mfloat\x1b[0m\x1b[49m:\x1b[0m "
+        "                              "
+        "    │\n     │ \x1b[49m    \x1b[0m\x1b[94"
+        ";49mreturn\x1b[0m\x1b[49m \x1b[0m\x1b[49mx"
+        "\x1b[0m\x1b[49m \x1b[0m\x1b[49m+\x1b[0m\x1b[49m "
+        "\x1b[0m\x1b[49my\x1b[0m                "
+        "                              "
+        "          │\n     ╰────────────"
+        "──────────────────────────────"
+        "──────────────────────────────"
+        "─╯\n"
+    )
+    assert output == expected_output
+
+
+def test_notebook_magic_code_cell(rich_output: RichOutput) -> None:
+    """It renders a code cell in a language specified by cell magic."""
+    code_cell = {
+        "cell_type": "code",
+        "execution_count": 3,
+        "id": "emotional-amount",
+        "metadata": {},
+        "outputs": [],
+        "source": "%%bash\necho 'lorep'",
+    }
+    expected_output = (
+        "\x1b[38;5;247m    \x1b[0m ╭─────────"
+        "─────╮\n\x1b[38;5;247m[3]:\x1b[0m │ \x1b"
+        "[49m%%\x1b[0m\x1b[94;49mbash\x1b[0m    "
+        "   │\n     │ \x1b[96;49mecho\x1b[0m\x1b["
+        "49m \x1b[0m\x1b[33;49m'lorep'\x1b[0m │\n"
+        "     │              │\n     ╰──"
+        "────────────╯\n"
+    )
+    output = rich_output(code_cell)
+    assert output == expected_output
+
+
+def test_notebook_plain_code_cell(rich_output: RichOutput) -> None:
+    """It renders a code cell with plain formatting."""
+    code_cell = {
+        "cell_type": "code",
+        "execution_count": 2,
+        "id": "emotional-amount",
+        "metadata": {},
+        "outputs": [],
+        "source": "def foo(x: float, y: float) -> float:\n    return x + y",
+    }
+    output = rich_output(code_cell, plain=True)
+    expected_output = (
+        "\x1b[94;49mdef\x1b[0m\x1b[49m \x1b[0m\x1b[92;"
+        "49mfoo\x1b[0m\x1b[49m(\x1b[0m\x1b[49mx\x1b[0m"
+        "\x1b[49m:\x1b[0m\x1b[49m \x1b[0m\x1b[96;49mfl"
+        "oat\x1b[0m\x1b[49m,\x1b[0m\x1b[49m \x1b[0m\x1b[4"
+        "9my\x1b[0m\x1b[49m:\x1b[0m\x1b[49m \x1b[0m\x1b[9"
+        "6;49mfloat\x1b[0m\x1b[49m)\x1b[0m\x1b[49m "
+        "\x1b[0m\x1b[49m-\x1b[0m\x1b[49m>\x1b[0m\x1b[49m "
+        "\x1b[0m\x1b[96;49mfloat\x1b[0m\x1b[49m:\x1b[0"
+        "m                             "
+        "              \n\x1b[49m    \x1b[0m\x1b["
+        "94;49mreturn\x1b[0m\x1b[49m \x1b[0m\x1b[49"
+        "mx\x1b[0m\x1b[49m \x1b[0m\x1b[49m+\x1b[0m\x1b[49"
+        "m \x1b[0m\x1b[49my\x1b[0m              "
+        "                              "
+        "                    \n"
+    )
+    assert output == expected_output
