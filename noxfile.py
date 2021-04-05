0852e46071944839e5a6fd68f1b682d58244980a
Paulo S. Costa <pauloscosta5@gmail.com>
2021-04-04 17:19:12 -0700

    Remove Python 3.6 references

--- noxfile.py
+++ noxfile.py
@@ -10,7 +10,7 @@ from nox_poetry import session
 
 
 package = "nbpreview"
-python_versions = ["3.9", "3.8", "3.7", "3.6"]
+python_versions = ["3.9", "3.8", "3.7"]
 nox.options.sessions = (
     "pre-commit",
     "safety",
