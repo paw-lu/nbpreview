d1de37a2e42ee96cb808b4fb8cd49980cac011a6
Paulo S. Costa <pauloscosta5@gmail.com>
2021-04-15 21:17:27 -0700

    :arrow_down: cookiecutter-hypermodern-python 2021.4.15

--- noxfile.py
+++ noxfile.py
@@ -5,8 +5,18 @@ from pathlib import Path
 from textwrap import dedent
 
 import nox
-from nox_poetry import Session
-from nox_poetry import session
+
+try:
+    from nox_poetry import Session
+    from nox_poetry import session
+except ImportError:
+    message = f"""\
+    Nox failed to import the 'nox-poetry' package.
+
+    Please install it using the following command:
+
+    {sys.executable} -m pip install nox-poetry"""
+    raise SystemExit(dedent(message))
 
 
 package = "nbpreview"
@@ -100,7 +110,7 @@ def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
     requirements = session.poetry.export_requirements()
     session.install("safety")
-    session.run("safety", "check", f"--file={requirements}", "--bare")
+    session.run("safety", "check", "--full-report", f"--file={requirements}")
 
 
 @session(python=python_versions)
