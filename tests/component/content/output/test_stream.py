3225a64cab2a233c5bcb609b9c9438fd60ba1165
Paulo S. Costa <pauloscosta5@gmail.com>
2021-08-07 20:16:47 -0700

    :white_check_mark: Test stream invalid StdErr name

---
+++ tests/component/content/output/test_stream.py
@@ -0,0 +1,12 @@
+"""Test for nbpreview.component.content.output.stream."""
+import nbformat
+import pytest
+
+from nbpreview.component.content.output import stream
+
+
+def test_invalid_stderr_name() -> None:
+    """It raises a ValueError when the name is not 'stderr'."""
+    notebook_output = nbformat.from_dict({"name": "unknown"})
+    with pytest.raises(ValueError):
+        stream.StdErr.from_output(notebook_output)
