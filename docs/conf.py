d3050e9d52bd00727730e32313be1b6249055af1
Paulo S. Costa <pauloscosta5@gmail.com>
2022-01-26 19:15:54 -0800

    :memo: Enable heading anchors

--- docs/conf.py
+++ docs/conf.py
@@ -14,6 +14,7 @@ extensions = [
     "sphinxext.opengraph",
 ]
 autodoc_typehints = "description"
+myst_heading_anchors = 2
 html_theme = "furo"
 pygments_style = "material"
 pygments_dark_style = "material"
