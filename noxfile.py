462a1079f8d44f56e9b8db8944ee1126ae8a4286
Paulo S. Costa <pauloscosta5@gmail.com>
2025-10-14 11:53:18 -0700

    Set 3.13 as default task runner

--- noxfile.py
+++ noxfile.py
@@ -24,7 +24,7 @@ except ImportError:
 
 
 package = "nbpreview"
-python_versions = ["3.12", "3.13", "3.11"]
+python_versions = ["3.13", "3.12", "3.11"]
 nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
